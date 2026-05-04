8a3cc30b266ed6d97dd6fc2f8f15474cc9ad5a35
Meriem Hammoudi Haro <meriemhammoudih@gmail.com>
2026-05-04 11:29:00 +0200

    created sharpness_detection file + added finding peaks algorithm + added data labelling code

---
+++ sharpness_detection_v1.m
@@ -0,0 +1,23 @@
+%% data labelling (no se muy bien como llamarlo)
+[x_out_t, y_data] = getpoints(sharpnessline); 
+x_data = seconds(x_out_s); % turns duration into double
+%% finding peak positions
+thld = 0.1; %a threshold to make up for background noise
+middle = y_data(2:end-1); %voltage values except for ends
+
+% neighbours of each point 
+% 'left' is points 1 to N-2. 'right' is points 3 to N.
+left_neighbors  = y_data(1:end-2); 
+right_neighbors = y_data(3:end);
+
+% compare values to their neighbours to find if it's a peak!
+is_peak = (middle > left_neighbors) & ...
+          (middle > right_neighbors) & ...
+          (middle > thld);
+
+% REMEMBER! we ignored the end values so our first value in "middle" is the second element!
+locs = find(is_peak) + 1;
+
+% corresponding positions
+pks = y_data(locs);
+peak_positions = x_data(locs);
