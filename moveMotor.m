3b4197ba751514faee53e07312b9e228d4e5f9e4
Meriem Hammoudi Haro <meriemhammoudih@gmail.com>
2026-05-12 10:57:45 +0200

    Added comments to moveMotor

--- moveMotor.m
+++ moveMotor.m
@@ -3,7 +3,9 @@ function moveMotor(targetPos,rangeScan, actualPos, motorObj)
 %   actualPos = moveMotor(targetPos) simulates a short move and returns
 %   the target as the actual position. If a `motorObj` is provided, the
 %   function will attempt to use it.
-steps_to_move = actualPos - targetPos 
+steps_to_move = actualPos - targetPos %number of steps the stepper motor has to move to reach ideal sharpness
+
+% Direction the stepper motor has to follow according to the distance
 if steps_to_move < 0
     dir = 1
 end
@@ -11,9 +13,11 @@ if steps_to_move > 0
     dir = 0
 end
 
+% Moving the motor
 for i = rangeScan[1]:rangeScan[2]:
     if i > 1
         motorObj.startMoving(dir, 1000, steps_to_move);
     end
 end
-actualPos = targetPos
+
+actualPos = targetPos % Re-iteration of current position
