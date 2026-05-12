cd77fa2de730055bbcf666ded8ca8cce6cb9c142
Meriem Hammoudi Haro <meriemhammoudih@gmail.com>
2026-05-12 10:47:40 +0200

    Added function moveMotor

--- moveMotor.m
+++ moveMotor.m
@@ -1,8 +1,19 @@
-function actualPos = moveMotor(targetPos, motorObj)
+function moveMotor(targetPos,rangeScan, actualPos, motorObj)
 % moveMotor  Move motor/stage to target position.
 %   actualPos = moveMotor(targetPos) simulates a short move and returns
 %   the target as the actual position. If a `motorObj` is provided, the
 %   function will attempt to use it.
-
-
+steps_to_move = actualPos - targetPos 
+if steps_to_move < 0
+    dir = 1
 end
+if steps_to_move > 0
+    dir = 0
+end
+
+for i = rangeScan[1]:rangeScan[2]:
+    if i > 1
+        motorObj.startMoving(dir, 1000, steps_to_move);
+    end
+end
+actualPos = targetPos
